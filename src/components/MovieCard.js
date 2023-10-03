3e9517b59cc15adf3dda160f1381cddc54b92486
Fella Kettani <fellakettani@gmail.com>
2023-10-03 16:40:32 +0100

    finished assignement

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,11 +4,13 @@ function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  descritption = "This is a great movie!"
 }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
+      <h3>{descritption}</h3>
       <small>{genres.join(", ")}</small>
     </div>
   );
